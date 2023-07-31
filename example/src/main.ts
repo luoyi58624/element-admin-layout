90ba141370c94366140f22a5c7776c7a0be7b81a
luoyi <luoyi58624@gmail.com>
2023-07-31 21:35:32 +0800

    package.json信息完善

--- example/src/main.ts
+++ example/src/main.ts
@@ -2,18 +2,15 @@ import { createApp } from 'vue'
 import router from './router'
 
 import 'uno.css'
-// import '@unocss/reset/tailwind-compat.css'
 
-import 'element-plus/theme-chalk/dark/css-vars.css';
 import { installAdminLayout } from 'element-admin-layout-components'
 
 import App from './App.vue'
 
 createApp(App)
-  .use(router)
-  .use(installAdminLayout, {
-    title: 'Luoyi后台管理系统',
-    logo: '/img/logo.png',
-    // navbarButtons: ['full_screen']
-  })
-  .mount('#app')
+	.use(router)
+	.use(installAdminLayout, {
+		title: 'Luoyi后台管理系统',
+		logo: 'img/logo.png'
+	})
+	.mount('#app')
