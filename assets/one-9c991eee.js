9a62af2f4489d522b21849b0550d02db0b1e7191
luoyi58624 <46178521+luoyi58624@users.noreply.github.com>
2023-07-31 15:05:16 +0000

    Deploying to example_pages from @ luoyi58624/element-admin-layout@496f1a63d76136ff0a19388922c3403d5f7abd88 🚀

---
+++ assets/one-9c991eee.js
@@ -0,0 +1,1 @@
+import{_ as o,r as t,o as n,c as r,b as c,a as s}from"./index-54451876.js";const a={},_=s("h1",null,"一级路由",-1);function l(d,i){const e=t("router-view");return n(),r("div",null,[_,c(e)])}const f=o(a,[["render",l]]);export{f as default};
